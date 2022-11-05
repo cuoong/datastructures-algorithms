35140a70475d5bb7e3501a334029904744109174
Cuong Nguyen <cuong@192.168.2.6>
2022-11-05 23:48:43 +0700

    Solution to rotate array

--- 2-Arrays/Q1-CyclicRotation-Question.playground/Contents.swift
+++ 2-Arrays/Q1-CyclicRotation-Question.playground/Contents.swift
@@ -18,7 +18,24 @@ import UIKit
 
 func solution(A: [Int], K: Int) -> [Int] {
     // do your work here...
-    return [Int]()
+    guard !A.isEmpty else { return [] }
+    guard K > 0 else { return A }
+    
+    var result = A
+    for _ in 1...K {
+        result = rotateArray(A: result)
+    }
+    return result
+}
+
+func rotateArray(A: [Int]) -> [Int] {
+    var newArray = Array<Int>(repeating: 0, count: A.count)
+    for i in 0..<A.count - 1 {
+        // Shift next index in array to the left
+        newArray[i+1] = A[i]
+    }
+    newArray[0] = A.last!
+    return newArray
 }
 
 solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
@@ -27,3 +44,23 @@ solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
 
 solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
 
+rotateArray(A: [1,2,3,4,5])
+
+// using stack
+
+func solution1(A: [Int], K: Int) -> [Int] {
+    guard !A.isEmpty else { return [] }
+    guard K > 0 else { return A }
+    
+    var result = A
+    for _ in 1...K {
+        let last = result.last!
+        result.insert(last, at: 0)
+        result.removeLast()
+    }
+    return result
+}
+
+solution1(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
+solution1(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
+solution1(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
